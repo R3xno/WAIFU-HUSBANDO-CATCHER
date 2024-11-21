d3c7199e5be2ff4b53b0f7ab4badd1be4b2a4ba4
R3xno <rexno6969@gmail.com>
2024-11-22 00:47:02 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,10 +2,10 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
+    OWNER_ID = "7053677913"
+    sudo_users = "7053677913", "7360886606"
+    GROUP_ID = -1002297665895
+    TOKEN = "8043077010:AAF4yj7WFTLqb07AIL9lPsbBRKkdQDDPiSY"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
